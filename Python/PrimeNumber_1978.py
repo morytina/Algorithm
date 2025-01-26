872a10df07e66fbf9f615f0f416ab83b82322a9c
morytina <jh6434.park@gmail.com>
2025-01-26 21:08:07 +0900

    C and python code update

--- Python/PrimeNumber_1978.py
+++ Python/PrimeNumber_1978.py
@@ -1,5 +1,6 @@
 def make_prime():
     is_prime = [True] * 1001  # 2부터 1000까지의 소수 여부를 저장할 리스트
+    is_prime[0] = is_prime[1] = False
     for i in range(2, 1001):
         if is_prime[i]:
             cur = i * 2
@@ -14,11 +15,8 @@ def main():
     
     # 입력 처리
     import sys
-    input = sys.stdin.read
-    data = input().splitlines()
-    
-    N = int(data[0])  # 첫 번째 줄: N 값
-    numbers = map(int, data[1].split())  # 두 번째 줄: 숫자 리스트
+    N = sys.stdin.readline()
+    numbers = list(map(int, input().split()))
     
     # 소수 개수 카운트
     ans = sum(1 for num in numbers if is_prime[num])
